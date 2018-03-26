07a5dcebcaac591711d02dbe641151dea016f97c
Aaron Akbari-Mort and jared lutz <aaron@>
2018-03-26 16:47:28 -0700

    add form and javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
 
+    var sentences = ["itemOne"];
+    var output = [];
+
+    sentences.forEach(function(sentence) {
+      var userInput = $("input#" + sentence).val();
+      var splits = userInput.split(" ");
+      output.push(splits);
+    });
+    output.forEach(function(word){
+      if (word.length >= 3){
+        $(".output").append("<li>" + word + "</li>")
+
+        // alert(word);
+      };
+    });
+
+  });
 });
